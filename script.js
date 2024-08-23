fa58cc5b364e866b49878c0613717afa3a25d6e0
miqueias199808@outlook.com <miqueias199808@outlook.com>
2024-08-23 18:25:09 +0200

    updapte 1.1

--- script.js
+++ script.js
@@ -18,9 +18,47 @@ menu.style.display = "flex"
   
 }
 
+// funcao criada para efeitos css ao carregar a pagina //
 document.addEventListener("DOMContentLoaded", function() {
   let container = document.querySelector(".container");
   container.style.opacity = 1;
 
 
 })
+
+// script do formulario //
+
+
+// funcao que limpa os checkbox quando o Index é carregado //
+window.onload = function() {
+  let checkboxes = document.querySelectorAll('input[name="option"]');
+  checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+  });
+};
+
+ //essa funcao serve para armazenar o pedido do cliente //
+function ssd(event) {
+  event.preventDefault();
+ 
+  let pedidoselecionado = document.querySelectorAll('input[name="option"]:checked'); // seleciona os valores do checkbox
+
+  if (pedidoselecionado.length === 0) {
+    alert("Se quieres continuar, seleciona un artículo mínimo!! ") //condicao criada para indentificar se os inputs foram selecionados, com uma mensagem de error
+    return;
+  }
+
+  let pedidoarmazenado = []; // array que será usado para armazenamento dos valores selecionados do checkbox //
+  pedidoselecionado.forEach((checkbox) => {
+    pedidoarmazenado.push(checkbox.value);
+  });  //funcao arrow que percorre os checkbox e envia os selecionados ao array pedidoarmazenado //
+
+ alert(pedidoarmazenado);
+
+ let checkboxes = document.querySelectorAll('input[name="option"]');
+            checkboxes.forEach((checkbox) => {
+                checkbox.checked = false; // limpa checkbox apos o envio //
+            });
+}
+
+
